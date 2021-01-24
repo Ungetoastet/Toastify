e17676d9976e09ee391fe6d951e26bbd18b9c4ca
Ungetoastet <robertescheer@gmail.com>
2021-01-24 21:13:01 +0100

    Added a lot of Error detection and management
    Added error detection
    Now renames and moves files into a folder
    Now using dictionary instead of json for yt fetch

--- toastify_dl.py
+++ toastify_dl.py
@@ -1,32 +1,87 @@
-import spotipy, os, youtube_dl, json
+import spotipy, os, youtube_dl, json, platform
 from spotipy.oauth2 import SpotifyClientCredentials
 from youtube_search import YoutubeSearch
 
+# Check for MacOS and Windows users
+if platform.system() == "Darwin":
+    print("!MacOS has not been tested and might just crash!")
+if platform.system() == "Windows":
+    print("!Windows has not been tested and might just crash!")
+
+# Load options
 f = open("./dl_opts.json")
 options = json.load(f)
 os.environ["SPOTIPY_CLIENT_ID"] = options["clientid"]
 os.environ["SPOTIPY_CLIENT_SECRET"] = options["clientsec"]
-
-playlist_id = input("Input Spotify Playlist URI: ")
-
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 ydl_opts = options["ydl_opts"]
 
+# Promp for Playlist URI
+playlist_id = input("Input Spotify Playlist URI: ")
+
+# Login to Spotify API
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+# Set error detection Variable
+flawless = True
+successfull = 0
+
+# Create download folder
+try:
+    os.mkdir("./Downloaded")
+except OSError:
+    print("Creation of the download folder failed. (Maybe the folder already exists)")
+    x = input("Continue? (y/n)")
+    if x != "y":
+        exit()
+
+# Fetch Playlist Tracks from Spotify
 results = sp.user_playlist_tracks("lol", playlist_id, fields='items,uri,name,id,total', market='fr')
+tracknum = len(results["items"])
+print(str(tracknum) + " songs found.")
+
+# Loop over Tracks and Download them
 for item in results["items"]:
+    #Create Trackname
     track_Name = item["track"]["artists"][0]["name"] + " - " + item["track"]["name"]
-    search_res = YoutubeSearch(track_Name, max_results=1).to_json()
-    video_id = search_res.split('"')[5]
-    video_name = search_res.split('"')[15]
-    print("Downloading song: " + track_Name + " with YT id " + video_id)
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        dl = ydl.download(['http://youtu.be/'+video_id])
-
+    #Get video from yt
     try:
-        dl_file_name = video_name+"-"+video_id.capitalize()
-        os.rename(r"./"+dl_file_name+".mp3",r"./"+track_Name+".mp3")
-        print("Song has been renamed.")
-    except FileNotFoundError:
-        print("Song couldn't be renamed.")
+        search_resdic = YoutubeSearch(track_Name, max_results=1).to_dict()
+    except KeyError:
+        print("Youtube equivalent could not be found for " + track_Name)
+        search_resdic = "Failed"
+        flawless = False
+    
+    if search_resdic != "Failed":
+        #Get video information
+        try:
+            video_id = search_resdic[0]["id"]
+            video_name = search_resdic[0]["title"]
+        except IndexError:
+            print("Youtube equivalent could not be found for " + track_Name)
+            video_id = "Failed"
+            flawless = False
+        
+        if video_id != "Failed":
+            print("\nDownloading song: " + track_Name)
+            #Download the video and convert to mp3
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(['http://youtu.be/'+video_id])
+            successfull += 1
+            #Rename and move the file
+            try:
+                dl_file_name = video_name+"-"+video_id
+                os.rename(r"./"+dl_file_name+".mp3",r"./Downloaded/"+track_Name+".mp3")
+                print("Song has been renamed and moved")
+            except FileNotFoundError:
+                print("Song couldn't be renamed.")
+                flawless = False
 
-print("\n\nPlaylist downloading complete!")
+# Present end result
+print("\n\n\n")
+if flawless:
+    print("Playlist downloading completed without errors.")
+else:
+    print("Playlist download completed with errors. Check Folder for missing songs or renames.")
+
+print("The songs can be found at: " + os.getcwd() + "/Downloaded")
+print("Downloaded " + str(successfull) + " out of " + str(tracknum) + " songs")
